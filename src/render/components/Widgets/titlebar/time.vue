dad60697d96b2b50cdcabfaa6394b24ef2934e2a
wingdust <995315823@qq.com>
2021-03-16 20:17:55 +0800

    feat: time compoent

---
+++ src/render/components/Widgets/titlebar/time.vue
@@ -0,0 +1,14 @@
+<template>
+    <span>{{time}}</span>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue"
+// const timeFromDate = (date:Date) => date.toTimeString().slice(0,8)
+const timeFromDate = (date:Date) => date.toTimeString().slice(0,5)
+let t:string=timeFromDate(new Date())
+setInterval(()=>{
+    t = timeFromDate(new Date())
+},1000*60)
+const time = ref(t);
+// export default {name:'time'}
+</script>
